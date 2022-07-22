f68ec6cd9925c8cf1520188230298423647058e8
Harry Hu <hoozh1123@gmail.com>
2022-07-22 15:45:29 +1000

    feat(): Add api routes.

--- server.js
+++ server.js
@@ -1,1 +1,44 @@
-console.log("server running...");
+import express from 'express';
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleWare from './middleware/error-handler.js';
+import dotenv from 'dotenv';
+import connectDB from './db/connect.js';
+import authRouter from './routes/authRoutes.js';
+import jobsRoutes from './routes/jobsRoutes.js';
+
+const app = express();
+dotenv.config();
+
+// db and authenticateUser
+
+// routers
+
+
+// middleware
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome!');
+})
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRoutes);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleWare);
+
+const port = process.env.PORT || 5001
+
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
